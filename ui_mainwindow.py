1c4869a286431e24b8b0a0cd7f8a01f75b7ff450
Jonas Pan <jonas.pan@signify.com>
2022-07-26 16:11:17 +0800

    add seperated lines for tool bar

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow, QPushButton,
-    QSizePolicy, QSpacerItem, QTextEdit, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QMainWindow,
+    QPushButton, QSizePolicy, QSpacerItem, QTextEdit,
+    QWidget)
 
 class Ui_Trailing(object):
     def setupUi(self, Trailing):
@@ -37,39 +38,23 @@ class Ui_Trailing(object):
         self.gridLayout.setSpacing(2)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(1, 1, 1, 1)
-        self.btn_uart_switch = QPushButton(self.centralwidget)
-        self.btn_uart_switch.setObjectName(u"btn_uart_switch")
+        self.btn_font = QPushButton(self.centralwidget)
+        self.btn_font.setObjectName(u"btn_font")
         sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.btn_uart_switch.sizePolicy().hasHeightForWidth())
-        self.btn_uart_switch.setSizePolicy(sizePolicy1)
-        self.btn_uart_switch.setMinimumSize(QSize(24, 24))
-        self.btn_uart_switch.setMaximumSize(QSize(24, 24))
-        self.btn_uart_switch.setBaseSize(QSize(24, 24))
-        self.btn_uart_switch.setStyleSheet(u"padding: 2px;")
-        icon1 = QIcon()
-        icon1.addFile(u"resources/start.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.btn_uart_switch.setIcon(icon1)
-        self.btn_uart_switch.setIconSize(QSize(18, 18))
-        self.btn_uart_switch.setFlat(True)
-
-        self.gridLayout.addWidget(self.btn_uart_switch, 0, 1, 1, 1)
-
-        self.btn_font = QPushButton(self.centralwidget)
-        self.btn_font.setObjectName(u"btn_font")
         sizePolicy1.setHeightForWidth(self.btn_font.sizePolicy().hasHeightForWidth())
         self.btn_font.setSizePolicy(sizePolicy1)
         self.btn_font.setMinimumSize(QSize(24, 24))
         self.btn_font.setMaximumSize(QSize(24, 24))
-        icon2 = QIcon()
-        icon2.addFile(u"resources/font.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.btn_font.setIcon(icon2)
+        icon1 = QIcon()
+        icon1.addFile(u"resources/font.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_font.setIcon(icon1)
         self.btn_font.setIconSize(QSize(18, 18))
         self.btn_font.setCheckable(True)
         self.btn_font.setFlat(True)
 
-        self.gridLayout.addWidget(self.btn_font, 0, 5, 1, 1)
+        self.gridLayout.addWidget(self.btn_font, 0, 7, 1, 1)
 
         self.btn_max_line = QPushButton(self.centralwidget)
         self.btn_max_line.setObjectName(u"btn_max_line")
@@ -77,14 +62,14 @@ class Ui_Trailing(object):
         self.btn_max_line.setSizePolicy(sizePolicy1)
         self.btn_max_line.setMinimumSize(QSize(24, 24))
         self.btn_max_line.setMaximumSize(QSize(24, 24))
-        icon3 = QIcon()
-        icon3.addFile(u"resources/max_line.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.btn_max_line.setIcon(icon3)
+        icon2 = QIcon()
+        icon2.addFile(u"resources/max_line.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_max_line.setIcon(icon2)
         self.btn_max_line.setIconSize(QSize(18, 18))
         self.btn_max_line.setCheckable(True)
         self.btn_max_line.setFlat(True)
 
-        self.gridLayout.addWidget(self.btn_max_line, 0, 4, 1, 1)
+        self.gridLayout.addWidget(self.btn_max_line, 0, 6, 1, 1)
 
         self.btn_uart_settings = QPushButton(self.centralwidget)
         self.btn_uart_settings.setObjectName(u"btn_uart_settings")
@@ -105,25 +90,21 @@ class Ui_Trailing(object):
 
         self.gridLayout.addWidget(self.btn_uart_settings, 0, 0, 1, 1)
 
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.btn_uart_switch = QPushButton(self.centralwidget)
+        self.btn_uart_switch.setObjectName(u"btn_uart_switch")
+        sizePolicy1.setHeightForWidth(self.btn_uart_switch.sizePolicy().hasHeightForWidth())
+        self.btn_uart_switch.setSizePolicy(sizePolicy1)
+        self.btn_uart_switch.setMinimumSize(QSize(24, 24))
+        self.btn_uart_switch.setMaximumSize(QSize(24, 24))
+        self.btn_uart_switch.setBaseSize(QSize(24, 24))
+        self.btn_uart_switch.setStyleSheet(u"padding: 2px;")
+        icon3 = QIcon()
+        icon3.addFile(u"resources/start.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_uart_switch.setIcon(icon3)
+        self.btn_uart_switch.setIconSize(QSize(18, 18))
+        self.btn_uart_switch.setFlat(True)
 
-        self.gridLayout.addItem(self.horizontalSpacer, 0, 6, 1, 1)
-
-        self.btn_screen_down = QPushButton(self.centralwidget)
-        self.btn_screen_down.setObjectName(u"btn_screen_down")
-        sizePolicy1.setHeightForWidth(self.btn_screen_down.sizePolicy().hasHeightForWidth())
-        self.btn_screen_down.setSizePolicy(sizePolicy1)
-        self.btn_screen_down.setMinimumSize(QSize(24, 24))
-        self.btn_screen_down.setMaximumSize(QSize(24, 24))
-        icon4 = QIcon()
-        icon4.addFile(u"resources/down.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.btn_screen_down.setIcon(icon4)
-        self.btn_screen_down.setIconSize(QSize(18, 18))
-        self.btn_screen_down.setCheckable(True)
-        self.btn_screen_down.setChecked(True)
-        self.btn_screen_down.setFlat(True)
-
-        self.gridLayout.addWidget(self.btn_screen_down, 0, 2, 1, 1)
+        self.gridLayout.addWidget(self.btn_uart_switch, 0, 1, 1, 1)
 
         self.textEdit = QTextEdit(self.centralwidget)
         self.textEdit.setObjectName(u"textEdit")
@@ -140,7 +121,7 @@ class Ui_Trailing(object):
         self.textEdit.setLineWrapMode(QTextEdit.NoWrap)
         self.textEdit.setReadOnly(True)
 
-        self.gridLayout.addWidget(self.textEdit, 1, 0, 1, 7)
+        self.gridLayout.addWidget(self.textEdit, 1, 0, 1, 9)
 
         self.btn_uart_clear = QPushButton(self.centralwidget)
         self.btn_uart_clear.setObjectName(u"btn_uart_clear")
@@ -148,13 +129,47 @@ class Ui_Trailing(object):
         self.btn_uart_clear.setSizePolicy(sizePolicy1)
         self.btn_uart_clear.setMinimumSize(QSize(24, 24))
         self.btn_uart_clear.setMaximumSize(QSize(24, 24))
-        icon5 = QIcon()
-        icon5.addFile(u"resources/clear.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.btn_uart_clear.setIcon(icon5)
+        icon4 = QIcon()
+        icon4.addFile(u"resources/clear.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_uart_clear.setIcon(icon4)
         self.btn_uart_clear.setIconSize(QSize(18, 18))
         self.btn_uart_clear.setFlat(True)
 
-        self.gridLayout.addWidget(self.btn_uart_clear, 0, 3, 1, 1)
+        self.gridLayout.addWidget(self.btn_uart_clear, 0, 4, 1, 1)
+
+        self.btn_screen_down = QPushButton(self.centralwidget)
+        self.btn_screen_down.setObjectName(u"btn_screen_down")
+        sizePolicy1.setHeightForWidth(self.btn_screen_down.sizePolicy().hasHeightForWidth())
+        self.btn_screen_down.setSizePolicy(sizePolicy1)
+        self.btn_screen_down.setMinimumSize(QSize(24, 24))
+        self.btn_screen_down.setMaximumSize(QSize(24, 24))
+        icon5 = QIcon()
+        icon5.addFile(u"resources/down.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_screen_down.setIcon(icon5)
+        self.btn_screen_down.setIconSize(QSize(18, 18))
+        self.btn_screen_down.setCheckable(True)
+        self.btn_screen_down.setChecked(True)
+        self.btn_screen_down.setFlat(True)
+
+        self.gridLayout.addWidget(self.btn_screen_down, 0, 3, 1, 1)
+
+        self.line = QFrame(self.centralwidget)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShadow(QFrame.Raised)
+        self.line.setFrameShape(QFrame.VLine)
+
+        self.gridLayout.addWidget(self.line, 0, 2, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer, 0, 8, 1, 1)
+
+        self.line_2 = QFrame(self.centralwidget)
+        self.line_2.setObjectName(u"line_2")
+        self.line_2.setFrameShadow(QFrame.Raised)
+        self.line_2.setFrameShape(QFrame.VLine)
+
+        self.gridLayout.addWidget(self.line_2, 0, 5, 1, 1)
 
         Trailing.setCentralWidget(self.centralwidget)
 
@@ -165,12 +180,11 @@ class Ui_Trailing(object):
 
     def retranslateUi(self, Trailing):
         Trailing.setWindowTitle(QCoreApplication.translate("Trailing", u"MainWindow", None))
-        self.btn_uart_switch.setText("")
         self.btn_font.setText("")
         self.btn_max_line.setText("")
         self.btn_uart_settings.setText(QCoreApplication.translate("Trailing", u"COM3\n"
 "115200 8N1", None))
-        self.btn_screen_down.setText("")
+        self.btn_uart_switch.setText("")
         self.textEdit.setMarkdown("")
         self.textEdit.setHtml(QCoreApplication.translate("Trailing", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
@@ -180,5 +194,6 @@ class Ui_Trailing(object):
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Segoe UI'; font-size:9pt;\"><br /></p></body></html>", None))
         self.textEdit.setPlaceholderText("")
         self.btn_uart_clear.setText("")
+        self.btn_screen_down.setText("")
     # retranslateUi
 
