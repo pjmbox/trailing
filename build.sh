9b2c61d5a5a147ebe6dd921c5fb4d49b093a990a
Jonas Pan <jonas.pan@signify.com>
2023-07-17 17:47:10 +0800

    add icon to exe file.

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 
 rm -rf dist
 #pyinstaller -F -w -i wind.ico setup.py
-pyinstaller -F -w -n trailing main.py
-cp cf_actions.yaml  cf_aliases.yaml  cf_config.yaml  cf_highlight.yaml  config.yaml  dist/
+pyinstaller -F -w -n trailing -i resources/trailing.png main.py
+cp cf_actions.yaml  cf_aliases.yaml  cf_config.yaml  cf_highlight.yaml dist/
 cp -a resources dist/
 rm -f trailing.spec
 rm -rf build
